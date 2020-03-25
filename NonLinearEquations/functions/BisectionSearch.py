b330b3267a0b12f810f06eeb5173eab1759646f0
Bass-Ocelot <jair07@outlook.com>
2020-03-25 18:39:39 -0500

    algoritmos bsuqueda biseccion y rgla falsa

---
+++ NonLinearEquations/functions/BisectionSearch.py
@@ -0,0 +1,48 @@
+import math
+
+class Bisseccion:
+    def __init__(self,xi,xs,tolerancia,iter):
+        self.xi = float(xi)
+        self.xs = float(xs)
+        self.tolerancia = tolerancia
+        self.iter = iter
+    
+
+    def operacion(self,xi,xs,tolerancia,iter,funcion):
+        fxi = funcion.evaluate(xi)
+        fxs = funcion.evaluate(xs)
+        if fxi == 0:
+            print(f"{xi} es una raiz")
+        elif fxs == 0:
+            print(f"{xs} es una raiz")
+        elif fxi *fxs < 0:
+            xm =float((xi+xs)/2)
+            fxm = funcion.evaluate(xm)
+           
+            contador = 1
+            error = tolerancia + 1
+            # print("++ XI ++ XS ++ XM ++ FXM ++")
+            # print("--------------------")
+            # print(f"++ {xi} ++ {xs} ++ {xm} ++ {fxm} ++")
+            while error > tolerancia and fxm != 0 and contador < iter:
+                if fxm * fxi < 0:
+                    xs = xm
+                    fxs = fxm
+                else:
+                    xi = xm
+                    fxi = fxm
+                xaux = xm
+                xm = (xi+xs)/2
+                fxm = funcion.evaluate(xm)
+                error = abs(xm-xaux)
+                contador+=1
+                # print(f"++ {xi} ++ {xs} ++ {xm} ++ {fxm} ++")
+            
+            if fxm == 0:
+                print(f"{xm} es raiz")
+            elif (error < tolerancia):
+                print(f"{xm} es una aprox. a una raiz con tolerancia = {tolerancia}")
+            else:
+                print(f"fracaso en {iter} iteraciones")
+        else:
+            print("el intervalo es inadecuado")
