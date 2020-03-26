5aae0c03d71b34ceddfbe3e449c03f5311866c53
Tomas Navarro <tdnavarrom@eafit.edu.co>
2020-03-25 21:19:23 -0500

    Added Bisection and False Rule

--- NonLinearEquations/functions/BisectionSearch.py
+++ NonLinearEquations/functions/BisectionSearch.py
@@ -1,26 +1,22 @@
 import math
 
-class Bisseccion:
-    def __init__(self,xi,xs,tolerancia,iter):
-        self.xi = float(xi)
-        self.xs = float(xs)
-        self.tolerancia = tolerancia
-        self.iter = iter
-    
+class Bisection:
+    def __init__(self):
+        self.values = []
 
-    def operacion(self,xi,xs,tolerancia,iter,funcion):
+    def evaluate(self,xi,xs,tolerancia,iter,funcion, type_error = 1):
         fxi = funcion.evaluate(xi)
         fxs = funcion.evaluate(xs)
         if fxi == 0:
-            print(f"{xi} es una raiz")
+            return f"{xi} es una raiz"
         elif fxs == 0:
-            print(f"{xs} es una raiz")
+            return f"{xs} es una raiz"
         elif fxi *fxs < 0:
             xm =float((xi+xs)/2)
             fxm = funcion.evaluate(xm)
-           
             contador = 1
             error = tolerancia + 1
+            self.values.append([xm, fxm, error])
             # print("++ XI ++ XS ++ XM ++ FXM ++")
             # print("--------------------")
             # print(f"++ {xi} ++ {xs} ++ {xm} ++ {fxm} ++")
@@ -34,15 +30,24 @@ class Bisseccion:
                 xaux = xm
                 xm = (xi+xs)/2
                 fxm = funcion.evaluate(xm)
-                error = abs(xm-xaux)
+
+                if type_error == 0: error = abs(xm-xaux)
+                else: error = abs((xm-xaux)/xm)
+
+                self.values.append([xm, fxm, error])
+
                 contador+=1
                 # print(f"++ {xi} ++ {xs} ++ {xm} ++ {fxm} ++")
-            
+
             if fxm == 0:
-                print(f"{xm} es raiz")
+                return f"{xm} es raiz"
             elif (error < tolerancia):
-                print(f"{xm} es una aprox. a una raiz con tolerancia = {tolerancia}")
+                return f"{xm} es una aprox. a una raiz con tolerancia = {tolerancia}"
             else:
-                print(f"fracaso en {iter} iteraciones")
+                return f"fracaso en {iter} iteraciones"
         else:
-            print("el intervalo es inadecuado")
+            return "el intervalo es inadecuado"
+
+
+    def tabla_values(self):
+        return self.values
