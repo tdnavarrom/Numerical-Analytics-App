c6921a32d989bd1e368c94d3053219e825e44607
ngrajale13 <57909195+ngrajale13@users.noreply.github.com>
2020-04-11 17:57:11 -0500

    Add files via upload

---
+++ NonLinearEquationsUI.py
@@ -0,0 +1,63 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from .functions.Incremental_Search_UI import incremental_search_ui as isui
+from .functions.Bisection_Search_UI import bisection_search_ui as bsui
+from .functions.FalseRule_Search_UI import falseRule_search_ui as fsui
+from .functions.FixedPoint_Search_UI import fixedPoint_search_ui as fpsui
+from .functions.NewtonSearch_UI import newton_search_ui as nsui
+from .functions.SecantSearch_UI import secant_search_ui as ssui
+
+class NonLinealMenu(Gtk.Notebook):
+
+    def __init__(self):
+        self.notebook = self.create_ui()
+
+
+    def create_ui(self):
+
+        notebook = Gtk.Notebook()
+        page1 = Gtk.Box()
+        page1.set_border_width(10)
+        page1.add(isui().grid)
+        notebook.append_page(page1, Gtk.Label('Incremental Search'))
+
+        page2 = Gtk.Box()
+        page2.set_border_width(10)
+        page2.add(bsui().grid)
+        notebook.append_page(page2, Gtk.Label('Bisection Search'))
+
+
+        page3 = Gtk.Box()
+        page3.set_border_width(10)
+        page3.add(fsui().grid)
+        notebook.append_page(page3, Gtk.Label('False-Rule Search'))
+
+        page4 = Gtk.Box()
+        page4.set_border_width(10)
+        page4.add(fpsui().grid)
+        notebook.append_page(page4, Gtk.Label('Fixed-Point Search'))
+
+        page5 = Gtk.Box()
+        page5.set_border_width(10)
+        page5.add(nsui().grid)
+        notebook.append_page(page5, Gtk.Label('Newton Search'))
+
+        page6 = Gtk.Box()
+        page6.set_border_width(10)
+        page6.add(ssui().grid)
+        notebook.append_page(page6, Gtk.Label('Secant Search'))
+
+
+        page7 = Gtk.Box()
+        page7.set_border_width(10)
+        page7.add(Gtk.Label('The other one'))
+        notebook.append_page(page7, Gtk.Label('Plain Title'))
+
+
+        page8 = Gtk.Box()
+        page8.set_border_width(10)
+        page8.add(Gtk.Label('The last one'))
+        notebook.append_page(page8, Gtk.Label('Plain Title'))
+
+        return notebook
