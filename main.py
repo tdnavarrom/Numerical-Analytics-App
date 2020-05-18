d27aea5c96b7a2d80567cb69b215cb49609d30bb
Tomas David Navarro Munera <tdnavarrom@eafit.edu.co>
2020-05-17 23:05:52 -0500

    Commit color css

--- main.py
+++ main.py
@@ -11,15 +11,8 @@ class App:
         self.builder = Gtk.Builder()
         self.builder.add_from_file("Prueba.glade")
 
-        css_provider = Gtk.CssProvider()
-        css_provider.load_from_path('main.css')
-        Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(),
-            css_provider,
-            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-
         go = self.builder.get_object
-        
+
         self.window = go("window1")
         self.method = go("Method")
         self.error = go("Error")
@@ -36,7 +29,17 @@ class App:
         self.parameters = self.method, self.error, self.help, self.function, self.gfunction, self.iterations, self.increment, self.initial, self.superior, self.result, self.tolerance
         self.builder.connect_signals(Handler(self.parameters))
 
-        self.window.show()
+        self.window.connect("destroy", Gtk.main_quit)
+        gtk_style()
+        self.window.show_all()
+
+
+def gtk_style():
+
+    screen = Gdk.Screen.get_default()
+    provider = Gtk.CssProvider()
+    provider.load_from_path("main.css")
+    Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
 
 if __name__ == '__main__':
